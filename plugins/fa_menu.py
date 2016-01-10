6600e8fdb32b70a7997fb4ebaad511e736ee4517
Michael Maurer <maurermj08@gmail.com>
2016-01-09 21:12:41 -0500

    Added a sample menu. Updated the look of Analyze, File Directory, and Overview plugins

--- plugins/fa_menu.py
+++ plugins/fa_menu.py
@@ -1,11 +1,11 @@
 """
-Gets an overview of the file without using cache
+Simple menu bar
 """
 
 from yapsy.IPlugin import IPlugin
 import os
 
-class FaOverview(IPlugin):
+class FaMenu(IPlugin):
 
     def __init__(self):
         IPlugin.__init__(self)
@@ -20,7 +20,7 @@ class FaOverview(IPlugin):
 
     def display_name(self):
         """Returns the name displayed in the webview"""
-        return "Overview"
+        return "Menu"
 
     def check(self, curr_file, path_on_disk, mimetype, size):
         """Checks if the file is compatable with this plugin"""
@@ -32,11 +32,11 @@ class FaOverview(IPlugin):
 
     def popularity(self):
         """Returns the popularity which is used to order the apps from 1 (low) to 10 (high), default is 5"""
-        return 10 
+        return 0
 
     def parent(self):
         """Returns if the plugin accepts other plugins (True) or only files (False)"""
-        return False
+        return True
 
     def cache(self):
         """Returns if caching is required"""
@@ -44,18 +44,16 @@ class FaOverview(IPlugin):
 
     def get(self, curr_file, helper, path_on_disk, mimetype, size, address, port, request, children):
         """Returns the result of this plugin to be displayed in a browser"""
-        
-        listing = []
-
-        for item in curr_file:
-            listing.append('<tr><td>' + str(item) + '</td><td>' + str(curr_file[item]) + '</td></tr>')
-
         html = ""
         curr_dir = os.path.dirname(os.path.realpath(__file__))
-        template = open(curr_dir + '/overview_template.html', 'r')
+        template = open(curr_dir + '/menu_template.html', 'r')
         html = str(template.read())
-        template.close()
 
-        html = html.replace('<!-- Table -->', '\n'.join(listing))
+        if request.query_string:
+            query_string = "?" + request.query_string
+        else:
+            query_string = ""
+
+        html = html.replace('<!-- Home -->', "http://" + address + ":" + port + "/plugins/" + children + query_string)
 
         return html
