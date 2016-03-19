8855a670529d6b37bc742fc828e06f2d629ace1b
Michael Maurer <maurermj08@gmail.com>
2016-03-18 21:35:59 -0400

    Updated install script to have Reglib

--- install.sh
+++ install.sh
@@ -27,4 +27,10 @@ sudo cp $DIR/misc/plaso/output/efetch.py /usr/lib/python2.7/dist-packages/plaso/
 sudo cp $DIR/misc/plaso/cli/helpers/efetch_output.py /usr/lib/python2.7/dist-packages/plaso/cli/helpers/.
 echo "from plaso.output import efetch" | sudo tee --append /usr/lib/python2.7/dist-packages/plaso/output/__init__.py
 echo "from plaso.cli.helpers import efetch_output" | sudo tee --append /usr/lib/python2.7/dist-packages/plaso/cli/helpers/__init__.py
+
+#Installing external dependencies
+wget https://github.com/williballenthin/python-registry/archive/master.zip
+unzip master.zip
+sudo python-registry-*/setup.py install
+
 echo 'Done!'
