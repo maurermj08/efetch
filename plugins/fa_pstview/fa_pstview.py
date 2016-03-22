3c51d1afd5fcf0ac346afacae1370da05bef77a9
Michael Maurer <maurermj08@gmail.com>
2016-03-21 22:50:02 -0400

    Added a basic PST plugin, no attachments yet

---
+++ plugins/fa_pstview/fa_pstview.py
@@ -0,0 +1,43 @@
+"""
+Lazy loading PST viewer
+"""
+
+from yapsy.IPlugin import IPlugin
+import os
+
+
+class FaPstview(IPlugin):
+    def __init__(self):
+        self.display_name = 'PST Viewer'
+        self.popularity = 8
+        self.parent = False
+        self.cache = True
+        IPlugin.__init__(self)
+
+    def activate(self):
+        IPlugin.activate(self)
+        return
+
+    def deactivate(self):
+        IPlugin.deactivate(self)
+        return
+
+    def check(self, evidence, path_on_disk):
+        """Checks if the file is compatable with this plugin"""
+        allowed_mimetype = ['application/octet-stream']
+        allowed_extensions = ['pst', 'pab', 'pff', 'ost', 'off']
+        return str(evidence['mimetype']).lower() in allowed_mimetype and evidence['ext'].lower() in allowed_extensions
+
+    def mimetype(self, mimetype):
+        """Returns the mimetype of this plugins get command"""
+        return "text/plain"
+
+    def get(self, evidence, helper, path_on_disk, request, children):
+        """Returns the result of this plugin to be displayed in a browser"""
+        curr_dir = os.path.dirname(os.path.realpath(__file__))
+        template = open(curr_dir + '/pstview_template.html', 'r')
+        html = str(template.read())
+        html = html.replace("<!-- Path -->", evidence['pid'])
+        template.close()
+
+        return html
