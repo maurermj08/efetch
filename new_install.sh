a2694e5d4259d12f2aff465c1233b5c46eaefcb3
maurermj08 <maurermj08@gmail.com>
2016-07-20 22:24:48 -0400

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -11,4 +11,4 @@ echo 'Installing Efetch dependencies...'
 sudo add-apt-repository -y ppa:gift/stable
 sudo add-apt-repository -y ppa:sift/stable
 sudo apt-get update
-sudo apt-get -y install python-plaso python-dev default-jre unoconv libpff libpff-python zlib1g-dev libjpeg-dev libtiff5-dev
+sudo apt-get -y install python-plaso python-dev python-setuptools unoconv libpff libpff-python zlib1g-dev libjpeg-dev libtiff5-dev
