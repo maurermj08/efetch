3729d52e8ce5a9296701e5b447788c68df884a08
Michael Maurer <maurermj08@gmail.com>
2016-08-09 22:38:16 -0400

    Added polling to YAML plugin file. Updated home page. Removed parent variable from all plugins.

---
+++ utils/poll.py
@@ -0,0 +1,24 @@
+import logging
+import os
+import threading
+import time
+
+
+class Poll(threading.Thread):
+    """This class polls the YAML plugin file for changes"""
+
+    def __init__(self, EfetchPluginManager, interval=3):
+        self._interval = interval
+        self._last_timestamp = 0
+        self._plugin_manager = EfetchPluginManager
+        threading.Thread.__init__(self)
+
+    def run(self):
+        """Polls the plugin file based on modification time and reloads plugins if there is a change"""
+        while True:
+            current_timestamp = os.stat(self._plugin_manager.plugins_file).st_mtime
+            if current_timestamp != self._last_timestamp:
+                logging.info(u'Plugin YAML file updated')
+                self._last_timestamp = current_timestamp
+                self._plugin_manager.reload_plugins_file()
+            time.sleep(self._interval)
