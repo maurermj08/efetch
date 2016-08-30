6d826c9bc02306176faeda024ef23bb7a64d6d87
Michael Maurer <maurermj08@gmail.com>
2016-08-29 23:12:13 -0400

    Major updates to action plugin, fixed scanning and multithreading.

--- efetch_server/plugins/experimental/fa_action_ajax.py
+++ efetch_server/plugins/experimental/fa_action_ajax.py
@@ -13,36 +13,40 @@ import uuid
 from bottle import abort
 from yapsy.IPlugin import IPlugin
 
+
 class SimpleRequest(object):
     """Pseudo Request object to pass to plugins"""
     pass
 
-def _action_process(items, helper, request, action_id, plugin, index, check, _actions):
+def _action_process(items, helper, request, action_id, plugin, index, check, _actions, action_lock):
     """Runs the specified plugin on the provided event items aka events[hits][hits]"""
     for item in items:
-        source = item['_source']
-        if 'pathspec' not in source:
-            # logging.warn('Missing pathspec, skipping document ' + item['_id'])
-            _actions[action_id]['current'] += 1.
-            _actions[action_id]['fail'] += 1
-        else:
-            efetch_dictionary = helper.get_efetch_dictionary(source['pathspec'], index, plugin.cache,
-                                                             hasattr(plugin, 'fast') and plugin.fast)
-            efetch_dictionary['_id'] = item['_id']
-            efetch_dictionary['doc_type'] = item['_type']
-            if not check or plugin.check(efetch_dictionary, efetch_dictionary['file_cache_path']):
-                try:
-                    plugin.get(efetch_dictionary, helper, efetch_dictionary['file_cache_path'], request)
-                    _actions[action_id]['success'] += 1
-                except Exception as e:
-                    # logging.debug('Plugin failed with message: ' + e.message)
+        try:
+            source = item['_source']
+            if 'pathspec' not in source:
+                with action_lock:
                     _actions[action_id]['fail'] += 1
+            else:
+                efetch_dictionary = helper.get_efetch_dictionary(source['pathspec'], index, plugin.cache,
+                                                                 hasattr(plugin, 'fast') and plugin.fast)
+                efetch_dictionary['_id'] = item['_id']
+                efetch_dictionary['doc_type'] = item['_type']
+                if not check or plugin.check(efetch_dictionary, efetch_dictionary['file_cache_path']):
 
-            _actions[action_id]['current'] += 1.
-            _actions[action_id]['percent'] = "{0:.0f}%".format(_actions[action_id]['current'] /
+                        plugin.get(efetch_dictionary, helper, efetch_dictionary['file_cache_path'], request)
+                with action_lock:
+                    _actions[action_id]['success'] += 1
+        except Exception as e:
+            with action_lock:
+                _actions[action_id]['fail'] += 1
+                _actions[action_id]['error'] += e.message + '\n\n'
+        finally:
+            with action_lock:
+                _actions[action_id]['current'] += 1
+                _actions[action_id]['percent'] = "{0:.0f}%".format(_actions[action_id]['current'] /
                                                                _actions[action_id]['total'] * 100)
-            if _actions[action_id]['current'] == _actions[action_id]['total']:
-                _actions[action_id]['status'] = 'done'
+                if _actions[action_id]['current'] == _actions[action_id]['total']:
+                    _actions[action_id]['status'] = 'done'
 
 class FaActionAjax(IPlugin):
 
@@ -52,8 +56,6 @@ class FaActionAjax(IPlugin):
         self.cache = False
         self.fast = True
         self._actions = {}
-        #self._max_processes = max([multiprocessing.cpu_count() - 1, 1])
-        #self._process_pool = multiprocessing.Pool(processes=self._max_processes, maxtasksperchild=100)
         self._thread_pool_size = max([multiprocessing.cpu_count() - 1, 1])
         self._thread_pool = ThreadPool(processes=self._thread_pool_size)
         self._max_queue_size = 100
@@ -95,7 +97,9 @@ class FaActionAjax(IPlugin):
                                         'success': 0,
                                         'fail': 0,
                                         'current': 0.,
-                                        'percent': '0'}
+                                        'percent': '0',
+                                        'error': '',
+                                        'total': 0}
             pseudo_request = SimpleRequest()
             pseudo_request.query = copy.deepcopy(request.query)
             pseudo_request.forms = copy.deepcopy(request.forms)
@@ -136,6 +140,7 @@ class FaActionAjax(IPlugin):
         order = helper.get_request_value(request, 'order', 'asc')
         all = bool(helper.get_request_value(request, 'all', False))
         check = helper.get_request_value(request, 'check', True)
+        action_lock = threading.Lock()
 
         if not index:
             self._actions[action_id]['status'] = "Action plugin requires an index, but none found"
@@ -157,34 +162,55 @@ class FaActionAjax(IPlugin):
 
         # Get Events
         query_body = helper.get_filters(request)
-        query_body['size'] = size
-        if sort:
+        if sort and not all:
             query_body['sort'] = {sort: order}
 
         if size > self._max_size or all:
+            query_body['size'] = 0
+            temp_events = [helper.db_util.elasticsearch.search(index=index, doc_type='plaso_event', body=query_body)]
+            self._actions[action_id]['total'] = temp_events[0]['hits']['total']
+            del query_body['size']
             events = elasticsearch.helpers.scan(helper.db_util.elasticsearch, query_body,
                                                 scroll=u'240m', size=self._max_size)
+            count = 0
+            for item in events:
+                count += 1
+                print('count: ' + str(count))
+                with action_lock:
+                    self._actions[action_id]['status'] = 'active'
+
+                self._queue.append(item)
+                if len(self._queue) >= self._max_queue_size:
+                    self._thread_pool.apply_async(_action_process, args=(self._queue, helper, request, action_id,
+                                                                         plugin, index, check,
+                                                                         self._actions, action_lock))
+                    self._queue = []
+
+                logging.info('QUEUE SIZE: ' + str(len(self._queue)))
         else:
+            query_body['size'] = size
             events = [helper.db_util.elasticsearch.search(index=index, doc_type='plaso_event', body=query_body)]
             self._actions[action_id]['total'] = min(events[0]['hits']['total'], int(size))
 
-        for event in events:
-            if size > self._max_size or all:
-                self._actions[action_id]['total'] += min(event['hits']['total'], self._max_size)
+            count = 0
+            for event in events:
+                with action_lock:
+                    self._actions[action_id]['status'] = 'active'
 
-            self._actions[action_id]['status'] = 'active'
+                # Run Events
+                for item in event['hits']['hits']:
+                    count += 1
+                    print('count: ' + str(count))
+                    self._queue.append(item)
+                    if len(self._queue) == self._max_queue_size:
+                        self._thread_pool.apply_async(_action_process, args=(self._queue, helper, request, action_id,
+                                                                             plugin, index, check,
+                                                                             self._actions, action_lock))
+                        self._queue = []
 
-            # Run Events
-            for item in event['hits']['hits']:
-                self._queue.append(item)
-                if len(self._queue) == self._max_queue_size:
-                    self._thread_pool.apply_async(_action_process, args=(self._queue, helper, request, action_id,
-                                                                          plugin, index, check, self._actions))
-                    logging.info('Running 100 events')
-                    self._queue = []
-
-            logging.info('QUEUE SIZE: ' + str(len(self._queue)))
+                logging.info('QUEUE SIZE: ' + str(len(self._queue)))
 
         if len(self._queue) > 0:
-            self._thread_pool.apply_async(_action_process, args=(self._queue, helper, request, action_id,
-                                                                    plugin, index, check, self._actions))
+            self._thread_pool.apply_async(_action_process, args=(self._queue, helper, request, action_id, plugin,
+                                                                 index, check, self._actions, action_lock))
+        self._queue = []
