102b3b50c8185650db2ced983dd9f5bc39104c55
Michael Maurer <maurermj08@gmail.com>
2016-10-20 21:11:43 -0400

    Minor bug fixes

--- install.sh
+++ install.sh
@@ -16,16 +16,26 @@
 
 
 #Check that the script is run as root
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
+#if [ "$(id -u)" != "0" ]; then
+#   echo "This script must be run as root" 1>&2
+#   exit 1
+#fi
+#
+##Install Repo Dependencies
+#echo 'Installing Efetch dependencies...'
+#sudo add-apt-repository -y ppa:gift/stable
+#sudo add-apt-repository -y ppa:sift/stable
+#sudo apt-get update
+#sudo dpkg -i python-efetch*.deb
+#sudo apt-get -f install
+#echo 'Done!'
 
-#Install Repo Dependencies
 echo 'Installing Efetch dependencies...'
 sudo add-apt-repository -y ppa:gift/stable
 sudo add-apt-repository -y ppa:sift/stable
 sudo apt-get update
-sudo dpkg -i python-efetch*.deb
+sudo apt-get -y install python-plaso python-dev python-setuptools unoconv libpff libpff-python zlib1g-dev libjpeg-dev libtiff5-dev python-pip
+python ${PWD}/setup.py build
+python ${PWD}/setup.py install
 sudo apt-get -f install
 echo 'Done!'
