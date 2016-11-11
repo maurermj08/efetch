f5ca1ed10ec638e2afb4bdf93ba1642c67f9506d
Michael Maurer <maurermj08@gmail.com>
2016-11-10 21:13:04 -0500

    Removing un-used CSS

--- efetch_server/plugins/fa_exif/fa_exif.py
+++ efetch_server/plugins/fa_exif/fa_exif.py
@@ -36,6 +36,8 @@ class FaExif(IPlugin):
         """Returns the mimetype of this plugins get command"""
         return "text/plain"
 
+
+
     def get(self, evidence, helper, path_on_disk, request):
         """Returns the result of this plugin to be displayed in a browser"""
         image = PIL.Image.open(path_on_disk)
@@ -51,6 +53,11 @@ class FaExif(IPlugin):
 
         for key, value in exif_data.iteritems():
             table.append('<tr><td>' + str(key) + '</td><td>' + str(value)  + '</td></tr>')
+            if key == 'GPSInfo':
+                lat_long = get_lat_lon(value)
+                table.append('<tr><td>Google Maps</td><td><a target="_blank" href="https://maps.google.com/' + \
+                             '?q=loc:' + str(lat_long[0]) + ',' + str(lat_long[1])
+                             + '">Link to Google Maps</a>')
 
         return '''
         <!DOCTYPE html>
@@ -114,3 +121,76 @@ class FaExif(IPlugin):
         </body>
         </html>
         '''
+
+
+def get_exif_data(image):
+    """Returns a dictionary from the exif data of an PIL Image item. Also converts the GPS Tags"""
+    exif_data = {}
+    info = image._getexif()
+    if info:
+        for tag, value in info.items():
+            decoded = PIL.ExifTags.TAGS.get(tag, tag)
+            if decoded == "GPSInfo":
+                gps_data = {}
+                for t in value:
+                    sub_decoded = PIL.ExifTags.GPSTAGS.get(t, t)
+                    gps_data[sub_decoded] = value[t]
+
+                exif_data[decoded] = gps_data
+            else:
+                exif_data[decoded] = value
+
+    return exif_data
+
+
+def _get_if_exist(data, key):
+    if key in data:
+        return data[key]
+
+    return None
+
+
+def _convert_to_degress(value):
+    """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format"""
+    d0 = value[0][0]
+    d1 = value[0][1]
+    d = float(d0) / float(d1)
+
+    m0 = value[1][0]
+    m1 = value[1][1]
+    m = float(m0) / float(m1)
+
+    s0 = value[2][0]
+    s1 = value[2][1]
+    s = float(s0) / float(s1)
+
+    return d + (m / 60.0) + (s / 3600.0)
+
+
+def get_lat_lon(gps_info_value):
+    """Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)"""
+    lat = 0
+    lon = 0
+
+    gps_info = {}
+    for t in gps_info_value:
+        sub_decoded = PIL.ExifTags.GPSTAGS.get(t, t)
+        gps_info[sub_decoded] = gps_info_value[t]
+
+    print(str(gps_info))
+
+    gps_latitude = _get_if_exist(gps_info, "GPSLatitude")
+    gps_latitude_ref = _get_if_exist(gps_info, 'GPSLatitudeRef')
+    gps_longitude = _get_if_exist(gps_info, 'GPSLongitude')
+    gps_longitude_ref = _get_if_exist(gps_info, 'GPSLongitudeRef')
+
+    if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
+        lat = _convert_to_degress(gps_latitude)
+        if gps_latitude_ref != "N":
+            lat = 0 - lat
+
+        lon = _convert_to_degress(gps_longitude)
+        if gps_longitude_ref != "E":
+            lon = 0 - lon
+
+    return lat, lon
